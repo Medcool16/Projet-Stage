47029d9c2e3868159affa434ad55bc4cc5cbe477
Medcool16 <mohamed16slaoui@gmail.com>
2023-07-03 14:23:50 +0100

    commit7

--- Car_Rent/resources/views/Client/home.blade.php
+++ Car_Rent/resources/views/Client/home.blade.php
@@ -6,24 +6,24 @@
 
 @section('content')
     <section>
-      <div class="blockquote-wrapper position-absolute">
+      {{-- <div class="blockquote-wrapper position-absolute">
         <div class="blockquote">
           <h1 class="text-light">
             Experience the Thrill of the Road with Our Rentals
            </h1>
            <a href="#" class="btn btn-warning book position-absolute">Book Now</a>
           </div>
-      </div> 
+      </div>  --}}
       <div class="slide">
         <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="1800">
-              <img src="images/bg.jpg" class="d-block w-100 h-50">
+              <img src="images/1.jpg" class="d-block w-100 h-50">
             </div>
             <div class="carousel-item" data-bs-interval="1800">
-              <img src="images/bg.jpg" class="d-block w-100 h-50">
+              <img src="images/3.jpg" class="d-block w-100 h-50">
             </div>
-            <div class="carousel-item" data-bs-interval="1800">
+            <div class="carousel-item" data-bs-interval="">
               <img src="images/bg.jpg" class="d-block w-100 h-50">
             </div>
           </div>
@@ -37,22 +37,3 @@
     </section>
 @endsection
 
-
-
-
-
-{{-- <div class="container-fluid">
-  <div class="row">
-          <div class="col-md-6">
-          </div>
-          <div class="col-md-6">
-            <div class="blockquote-wrapper">
-              <div class="blockquote">
-                <h1>
-                  Experience the Thrill of the Road with Our Rentals
-                 </h1>
-              </div>
-            </div> 
-          </div>
-  </div>
-  </div> --}}
--- Car_Rent/resources/views/master/Homepage.blade.php
+++ Car_Rent/resources/views/master/Homepage.blade.php
@@ -8,58 +8,48 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/Css/homepage-nav-color.css">
+
     <title>@yield('title')</title>
 
     @yield('style')
+
 </head>
 
 <body>
 
-  <nav class="navbar bg-body-tertiary idk bg-dark">
+  <nav class="navbar navbar-expand-lg p-0 bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand text-light mx-3" href="#">
-            <div class="container">
-                <img src="images/carlogo.png" alt="Logo" width="100" height="90" class="">
-                <span>CarZilla</span>
-            </div>
-          </a>
-            <div class="d-flex justify-content-center flex-grow-1 "  style="margin: 0px 0px 8px -148px;">
-              <ul class="nav nav-tabs ul-mode mx-2">
-    
-                <li class="nav-item mx-1" role="presentation">
-                  <a href="{{ url('/home') }}" class="nav-link text-" aria-selected="true">Home</a>
-                </li>
-      
-                <li class="nav-item mx-1" role="presentation">
-                  <a href="{{ url('') }}" class="nav-link text-" aria-selected="false" >Park</a>
-                </li>
-      
-                <li class="nav-item mx-1" role="presentation">
-                  <a href="{{ url('/specials') }}" class="nav-link text-" aria-selected="false" >Specials</a>
-                </li>
-      
-                <li class="nav-item mx-1" role="presentation">
-                  <a href="" class="nav-link text-" aria-selected="false" >Our Agencies</a>
-                </li>
-      
-                <li class="nav-item mx-1" role="presentation">
-                  <a href="" class="nav-link text-" aria-selected="false" >About</a>
-                </li>
-      
-                <li class="nav-item mx-1" role="presentation">
-                  <a href="" class="nav-link text-" aria-selected="false" >Contact</a>
-                </li>
-      
-              </ul>
-          </div>
-          <div class="mx-2">
-            <a class="btn btn-outline-info">
-              <img src="/assets/person.svg" style="filter: invert(100%); width: 25px; height: 25px;">
-              Login</a>
-            {{-- <a class="btn btn-outline-warning">Login</a> --}}
-          </div>
+      <div class="d-flex justify-content-start">
+        <a class="navbar-brand mx-lg-5" href="#"><img src="/images/carlogo.png"></a>
+      </div>
+  
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+  
+      <div class="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo03">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active text-light" aria-current="page" href="{{ url('/home') }}">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="#">Park</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="{{ url('/specials') }}" >Specials</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="#" >Our Agencies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="#" >Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
-</nav>
+  </nav>
+  
+  
 
 <div>
   @yield('content')
